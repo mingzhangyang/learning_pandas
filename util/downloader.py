3ea350644182598d8a87e7c2df17ef8b0dc18ca2
Mingzhang Yang <redondoself@gmail.com>
2017-10-10 15:58:30 -0400

    jsd

--- util/downloader.py
+++ util/downloader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import urllib.request
 import sys
+import time
 
 def parseFile(path):
     urls = []
@@ -14,12 +15,16 @@ def parseFile(path):
 
 def download(arr):
     for url in arr:
+        start = time.time()
         file_name = url.split('/')[-1]
         urllib.request.urlretrieve(url, file_name)
-    print("Download completed!")
+        t = time.time()
+        print("%s [%.2f s]" % (file_name, t - start))
 
 if __name__ == '__main__':
     # print('Hello World')
     p = sys.argv[1]
     # print(p)
+    begin = time.time()
     download(parseFile(p))
+    print("Total time used: %.2f s\n" % (time.time() - begin))
